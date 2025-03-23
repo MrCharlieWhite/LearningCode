48df430fe947fa6e43431dcff013fc865f923be1
Charlie White <159645979+MrCharlieWhite@users.noreply.github.com>
2025-03-23 21:03:21 +0000

    Update Tip calculator.py
    Changed function names, added spacing.

--- Projects_2025/Tip calculator.py
+++ Projects_2025/Tip calculator.py
@@ -2,7 +2,7 @@
 # The program will calculate and display the total amount to be paid, including the tip.
 
 # Function that will calculate the tip to be added on
-def meal_total(meal_cost,tip_percentage):
+def get_tip_value(meal_cost,tip_percentage):
     tip_result = (tip_percentage / 100) * meal_cost + meal_cost
     return tip_result
 
@@ -13,19 +13,24 @@ while True:
     except:
         print("Please enter a valid option.")
         continue
+
     try:
         tip_percentage = float(input("Please enter the percentage of the meal price you would like to tip: "))
     except:
         print("Please enter a valid option.")
         continue
+
     print(f"Your meal was £{meal_cost} and you wish to tip {tip_percentage}%")
     confirmation = input("Is this correct, y or n: ")
+
     if confirmation == "y":
-        total_cost = meal_total(meal_cost, tip_percentage)
+        total_cost = get_tip_value(meal_cost, tip_percentage)
         print(f"Your meal cost £{meal_cost} and you wish to tip {tip_percentage}%, so your total comes to £{total_cost}.")
+
     elif confirmation == "n":
         print("Please re-enter details")
         continue
+
     else:
         print("Please choose a valid option")
         continue
